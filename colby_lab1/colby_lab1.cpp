76101fad8b82e847da1632ac18b068038399868f
BPOWEL11 <bpowel11@aum.edu>
2026-04-06 11:31:21 -0500

    Added output line by Benjamin Powell

--- colby_lab1/colby_lab1.cpp
+++ colby_lab1/colby_lab1.cpp
@@ -9,6 +9,7 @@ int main()
 {
     cout << "Hello from Colby! This is my first commit." << endl;
     cout << "Git makes collaboration easy!" << endl;
+    cout << "Changes made by Benjamin!" << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
